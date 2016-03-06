9f7a22dae57546dd8c1cb4abe137978b016eb75f
ndaruoke <rahasia1>
2016-03-06 13:31:05 +0700

    delete unused file and method

---
+++ public/js/config.js
@@ -0,0 +1,132 @@
+/**
+ * E-Patungan - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
+
+    // Configure Idle settings
+    IdleProvider.idle(5); // in seconds
+    IdleProvider.timeout(120); // in seconds
+
+    $urlRouterProvider.otherwise("/dashboards/landing");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('dashboards', {
+            abstract: true,
+            url: "/dashboards",
+            templateUrl: "views/common/content-main.html",
+        })
+        .state('dashboards.landing', {
+            url: "/landing",
+            templateUrl: "views/landing.html",
+            data: { pageTitle: 'Dashboard', specialClass: 'landing-page' }
+        })
+        .state('dashboards.product_details', {
+            url: "/product_details",
+            templateUrl: "views/product_details.html",
+            data: {pageTitle: 'Project Detail', specialClass: 'landing-page'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['css/plugins/slick/slick.css','css/plugins/slick/slick-theme.css','js/plugins/slick/slick.min.js']
+                        },
+                        {
+                            name: 'slick',
+                            files: ['js/plugins/slick/angular-slick.min.js']
+                        }
+                    ]);
+                }
+            }
+
+        })
+        .state('dashboards.payments', {
+            url: "/payments",
+            controller : "paymentCtrl",
+            templateUrl: "views/payments.html",
+            data: { pageTitle: 'Payment', specialClass: 'landing-page'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'ui.knob',
+                            files: ['js/plugins/jsKnob/jquery.knob.js','js/plugins/jsKnob/angular-knob.js']
+                        },
+                        {
+                            files: ['css/plugins/ionRangeSlider/ion.rangeSlider.css','css/plugins/ionRangeSlider/ion.rangeSlider.skinFlat.css','js/plugins/ionRangeSlider/ion.rangeSlider.min.js']
+                        },
+                        {
+                            insertBefore: '#loadBefore',
+                            name: 'localytics.directives',
+                            files: ['css/plugins/chosen/chosen.css','js/plugins/chosen/chosen.jquery.js','js/plugins/chosen/chosen.js']
+                        },
+                        {
+                            name: 'nouislider',
+                            files: ['css/plugins/nouslider/jquery.nouislider.css','js/plugins/nouslider/jquery.nouislider.min.js','js/plugins/nouslider/angular-nouislider.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/angular-datepicker.js']
+                        },
+                        {
+                            files: ['js/plugins/jasny/jasny-bootstrap.min.js']
+                        },
+                        {
+                            files: ['css/plugins/clockpicker/clockpicker.css', 'js/plugins/clockpicker/clockpicker.js']
+                        },
+                        {
+                            name: 'ui.switchery',
+                            files: ['css/plugins/switchery/switchery.css','js/plugins/switchery/switchery.js','js/plugins/switchery/ng-switchery.js']
+                        },
+                        {
+                            name: 'colorpicker.module',
+                            files: ['css/plugins/colorpicker/colorpicker.css','js/plugins/colorpicker/bootstrap-colorpicker-module.js']
+                        },
+                        {
+                            name: 'ngImgCrop',
+                            files: ['js/plugins/ngImgCrop/ng-img-crop.js','css/plugins/ngImgCrop/ng-img-crop.css']
+                        },
+                        {
+                            serie: true,
+                            files: ['js/plugins/moment/moment.min.js', 'js/plugins/daterangepicker/daterangepicker.js', 'css/plugins/daterangepicker/daterangepicker-bs3.css']
+                        },
+                        {
+                            name: 'daterangepicker',
+                            files: ['js/plugins/daterangepicker/angular-daterangepicker.js']
+                        },
+                        {
+                            files: ['css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css']
+                        },
+                        {
+                            name: 'ui.select',
+                            files: ['js/plugins/ui-select/select.min.js', 'css/plugins/ui-select/select.min.css']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('dashboards.success', {
+            url:"/success",
+            templateUrl: "views/success.html",
+            data: {pageTitle: 'payment success', specialClass: 'canvas-menu' }
+        })
+
+
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
