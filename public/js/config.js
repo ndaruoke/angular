abf6749d1c3b673f8a224b98ddc4af9db0850ff0
ndaruoke <rahasia1>
2016-03-10 09:23:33 +0700

    add some ui
    login
    register
    start project

--- public/js/config.js
+++ public/js/config.js
@@ -121,6 +121,126 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
             templateUrl: "views/success.html",
             data: {pageTitle: 'payment success', specialClass: 'canvas-menu' }
         })
+        .state('dashboards.register', {
+            url: "/register",
+            templateUrl: "views/register.html",
+            data: { pageTitle: 'Register', specialClass: 'landing-page'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('dashboards.login', {
+            url: "/login",
+            templateUrl: "views/login.html",
+            data: { pageTitle: 'Register', specialClass: 'landing-page'}
+        })
+        .state('dashboards.start', {
+            url: "/start",
+            templateUrl: "views/form_start_project.html",
+            controller: wizardCtrl,
+            data: { pageTitle: 'Buat Proyek', specialClass: 'landing-page'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['css/plugins/steps/jquery.steps.css']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('dashboards.start.start_one', {
+            url: '/step_one',
+            templateUrl: 'views/wizard/start_one.html',
+            data: { pageTitle: 'Wizard form' },
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            name: 'ui.knob',
+                            files: ['js/plugins/jsKnob/jquery.knob.js','js/plugins/jsKnob/angular-knob.js']
+                        },
+                        {
+                            files: ['css/plugins/ionRangeSlider/ion.rangeSlider.css','css/plugins/ionRangeSlider/ion.rangeSlider.skinFlat.css','js/plugins/ionRangeSlider/ion.rangeSlider.min.js']
+                        },
+                        {
+                            insertBefore: '#loadBefore',
+                            name: 'localytics.directives',
+                            files: ['css/plugins/chosen/chosen.css','js/plugins/chosen/chosen.jquery.js','js/plugins/chosen/chosen.js']
+                        },
+                        {
+                            name: 'nouislider',
+                            files: ['css/plugins/nouslider/jquery.nouislider.css','js/plugins/nouslider/jquery.nouislider.min.js','js/plugins/nouslider/angular-nouislider.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css','js/plugins/datapicker/angular-datepicker.js']
+                        },
+                        {
+                            files: ['js/plugins/jasny/jasny-bootstrap.min.js']
+                        },
+                        {
+                            files: ['css/plugins/clockpicker/clockpicker.css', 'js/plugins/clockpicker/clockpicker.js']
+                        },
+                        {
+                            name: 'ui.switchery',
+                            files: ['css/plugins/switchery/switchery.css','js/plugins/switchery/switchery.js','js/plugins/switchery/ng-switchery.js']
+                        },
+                        {
+                            name: 'colorpicker.module',
+                            files: ['css/plugins/colorpicker/colorpicker.css','js/plugins/colorpicker/bootstrap-colorpicker-module.js']
+                        },
+                        {
+                            name: 'ngImgCrop',
+                            files: ['js/plugins/ngImgCrop/ng-img-crop.js','css/plugins/ngImgCrop/ng-img-crop.css']
+                        },
+                        {
+                            serie: true,
+                            files: ['js/plugins/moment/moment.min.js', 'js/plugins/daterangepicker/daterangepicker.js', 'css/plugins/daterangepicker/daterangepicker-bs3.css']
+                        },
+                        {
+                            name: 'daterangepicker',
+                            files: ['js/plugins/daterangepicker/angular-daterangepicker.js']
+                        },
+                        {
+                            files: ['css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css']
+                        },
+                        {
+                            name: 'ui.select',
+                            files: ['js/plugins/ui-select/select.min.js', 'css/plugins/ui-select/select.min.css']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('dashboards.start.start_two', {
+            url: '/step_two',
+            templateUrl: 'views/wizard/start_two.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+        .state('dashboards.start.start_three', {
+            url: '/step_three',
+            templateUrl: 'views/wizard/start_three.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+        .state('dashboards.start.start_four', {
+            url: '/step_four',
+            templateUrl: 'views/wizard/start_four.html',
+            data: { pageTitle: 'Wizard form' }
+        })
+        .state('dashboards.start.start_five', {
+            url: '/step_five',
+            templateUrl: 'views/wizard/start_five.html',
+            data: { pageTitle: 'Wizard form' }
+        })
 
 
 }
